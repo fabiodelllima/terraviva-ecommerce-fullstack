b050c41f47f2c0b381eb3b5ad595cf2f8665a23c
fabiodelllima <f.f.fabiodelima@gmail.com>
2026-01-30 20:27:37 -0300

    refactor(product): Use selectors in views
    - Thin controllers delegating queries to selectors

--- terraviva/backend/apps/product/views.py
+++ terraviva/backend/apps/product/views.py
@@ -1,53 +1,53 @@
-from django.db.models import Q
+"""
+Product views - Controller layer.
+
+Thin controllers that delegate queries to selectors.
+"""
+
 from django.http import Http404
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-from .models import Category, Product
+from . import selectors
 from .serializers import CategorySerializer, ProductSerializer
 
 
 class LatestProductsList(APIView):
+    """List latest products."""
+
     def get(self, request, format=None):
-        products = Product.objects.all()[0:8]
+        products = selectors.get_latest_products(limit=8)
         serializer = ProductSerializer(products, many=True)
         return Response(serializer.data)
 
 
 class ProductDetail(APIView):
-    def get_object(self, category_slug, product_slug):
-        try:
-            return Product.objects.filter(category__slug=category_slug).get(slug=product_slug)
-        except Product.DoesNotExist:
-            raise Http404 from None
+    """Get product details by category and product slugs."""
 
     def get(self, request, category_slug, product_slug, format=None):
-        product = self.get_object(category_slug, product_slug)
+        product = selectors.get_product_by_slugs(category_slug, product_slug)
+        if not product:
+            raise Http404
         serializer = ProductSerializer(product)
         return Response(serializer.data)
 
 
 class CategoryDetail(APIView):
-    def get_object(self, category_slug):
-        try:
-            return Category.objects.get(slug=category_slug)
-        except Category.DoesNotExist:
-            raise Http404 from None
+    """Get category details with products."""
 
     def get(self, request, category_slug, format=None):
-        category = self.get_object(category_slug)
+        category = selectors.get_category_by_slug(category_slug)
+        if not category:
+            raise Http404
         serializer = CategorySerializer(category)
         return Response(serializer.data)
 
 
 @api_view(["POST"])
 def search(request):
+    """Search products by name or description."""
     query = request.data.get("query", "")
-
-    if query:
-        products = Product.objects.filter(Q(name__icontains=query) | Q(description__icontains=query))
-        serializer = ProductSerializer(products, many=True)
-        return Response(serializer.data)
-    else:
-        return Response({"products": []})
+    products = selectors.search_products(query)
+    serializer = ProductSerializer(products, many=True)
+    return Response(serializer.data)
